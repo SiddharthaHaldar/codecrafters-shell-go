ba010cb0d6b6122cdb449e0229d104c7829a7fac
Siddhartha Haldar <siddharthahaldar@Siddharthas-MacBook-Pro.local>
2025-12-09 16:09:03 -0800

    Stage 5

--- app/main.go
+++ app/main.go
@@ -12,8 +12,11 @@ func main() {
 		// Wait for user input
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			// fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
+		}
+		if command == "exit\n" {
+			os.Exit(0)
 		}
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
