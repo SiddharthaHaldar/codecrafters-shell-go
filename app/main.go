6130cab43a6373b6eacbf23556bcb8d6d2d580b7
Siddhartha Haldar <siddharthahaldar@Siddharthas-MacBook-Pro.local>
2025-12-09 16:46:52 -0800

    Stage 7

--- app/main.go
+++ app/main.go
@@ -1,13 +1,20 @@
 package main
+
 import (
 	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
+
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 func main() {
+	builtinCommands := make(map[string]struct{})
+	builtinCommands["echo"] = struct{}{}
+	builtinCommands["type"] = struct{}{}
+	builtinCommands["exit"] = struct{}{}
+
 	for {
 		fmt.Print("$ ")
 		// Wait for user input
@@ -22,6 +29,14 @@ func main() {
 			fmt.Println()
 		} else if strings.HasPrefix(command, "echo ") {
 			fmt.Print(command[5:len(command)-1]+"\n")
+		} else if command == "type\n" {
+		} else if strings.HasPrefix(command, "type ") {
+			com := strings.TrimSpace(command[5:len(command)-1])
+			if _, exists := builtinCommands[com]; exists {
+				fmt.Printf("%s is a shell builtin\n", com)
+			} else {
+				fmt.Println(com + ": not found")
+			}
 		} else{
 			fmt.Println(command[:len(command)-1] + ": command not found")
 		}
