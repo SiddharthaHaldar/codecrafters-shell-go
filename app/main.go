7f2d1e417dfdbb092dde4590e61927276f9fc3da
Siddhartha Haldar <siddharthahaldar@Siddharthas-MacBook-Pro.local>
2025-12-09 16:06:29 -0800

    Stage 4

--- app/main.go
+++ app/main.go
@@ -7,12 +7,14 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 func main() {
-	fmt.Print("$ ")
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		// fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Print("$ ")
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			// fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
